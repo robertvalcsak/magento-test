eb4ab056fdd2f63c3a40d9293f92a643801cbb4c
Róbert Valcsák <robertvalcsak93@gmail.com>
2023-06-06 16:57:26 +0200

    first commit

---
+++ code/Custom/TestWidget/Block/Widget/TestWidget.php
@@ -0,0 +1,7 @@
+<?php
+namespace Custom\TestWidget\Block\Widget;
+
+class TestWidget extends \Magento\Framework\View\Element\Template implements \Magento\Widget\Block\BlockInterface
+{
+    protected $_template = 'widget/testwidget.phtml';
+}
---
+++ code/Custom/TestWidget/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Custom_TestWidget',
+    __DIR__
+);
